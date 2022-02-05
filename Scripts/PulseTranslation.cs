b71d55fcf01824684df3aa2cb4d81838ec18f725
Daniel Barnes <81057377+danielzbarnes@users.noreply.github.com>
2022-02-05 12:18:18 -0800

    Update PulseTranslation.cs

--- Scripts/PulseTranslation.cs
+++ Scripts/PulseTranslation.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// a simple script similiar to the laser scripts
+// animates a pulse of light that slides along a light pillar
+// uses the same principal as the animated lasers, toggling between play/pause animation clips
 public class PulseTranslation : MonoBehaviour
 {
-    private float speed;
-    private float offset;
-    private Animator anim;
+	// multipliers applied to animation clip playback speed
+    private float speed;	// the speed the pulse will climb the light pillar
+    private float offset;	// the delay until the effect restarts
+    private Animator anim;	// the Animator control component
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,8 @@ public class PulseTranslation : MonoBehaviour
         UpdatePulse();
     }
 
+	// this sets the movement speed and delay of the pulse effect
+	// it's triggered by an Animation Event when the pulse_play animation clip completes
     public void UpdatePulse()
     {
         speed = Rand(0.5f, 1.5f);
@@ -23,6 +29,7 @@ public class PulseTranslation : MonoBehaviour
         anim.SetFloat("pulse_speed", speed);
         anim.SetFloat("pulse_offset", offset);
     }
+
 
     private float Rand(float min, float max)
     {
